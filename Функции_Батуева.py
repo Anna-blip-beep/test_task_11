48fc9470319450895f3f6e7396430155c38951cd
Anna-blip-beep <m2409217@edu.misis.ru>
2025-06-16 10:27:20 +0300

    Add files via upload

---
+++ Функции_Батуева.py
@@ -0,0 +1,43 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "20676443-78dd-43e9-8e21-c14915e518c3",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#Функции из кода\n",
+    "* analizar_palabra(palabra)\n",
+    "* welcome(message)\n",
+    "* request_word(message)\n",
+    "* process_word(message)\n",
+    "* request_conjugation_word(message)\n",
+    "* open_conjugation_site(message)\n",
+    "* request_declension_word(message)\n",
+    "* open_declension_site(message)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
